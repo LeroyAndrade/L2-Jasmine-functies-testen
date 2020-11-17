05b36314afb21a862718433e82d241612a2d03c2
AndradeLeroy <47416005+AndradeLeroy@users.noreply.github.com>
2020-11-17 23:55:11 +0100

    Niet in gebruik code verwijderd

--- webshop-LeroyAndrade/scripts/shoppingcart.js
+++ webshop-LeroyAndrade/scripts/shoppingcart.js
@@ -1,33 +1,3 @@
-function Dagomzet() {
- let omzet = 0;
-
- this.voegToeAanDagomzet = function (klantTotaal) {
-     omzet = omzet + klantTotaal;
- }
-
-}
-
-function Winkelmand() {
- let totaalBedrag = 0;
-
- this.getTotaalBedrag = function () {
-     return totaalBedrag;
- }
-
- this.vulWinkelmand = function (aantalSnoep, aantalChocola, aantalKoek) {
-
-     let snoep = new Snoep(aantalSnoep);
-     let chocola = new Chocola(aantalChocola);
-     let koek = new Koek(aantalKoek);
-
-     this.setWinkelmandBedrag(snoep, chocola, koek);
- }
-
- this.setWinkelmandBedrag = function (snoep, chocola, koek) {
-     totaalBedrag = totaalBedrag + snoep.getTotaal();
- }
-}
-
 function Snoep(){
 
 const prijs = 2.45;
@@ -63,7 +33,6 @@ const merk = 'Ritter Sport';
 
 }
 
-
 //Koppel de variabel snoep, aan het Object Snoep
 let snoep = new Snoep();
 console.log(snoep.getPrijs());
@@ -75,68 +44,7 @@ console.table(snoep.getMerk());
 console.log(snoep.getSoortChoco(0));
 
 
-console.table(snoep.getAllChocoNames());
+console.log(snoep.getAllChocoNames());
 
 
 console.table(snoep.getGewicht());
-
-
-
-
-
-
-
-
-
-/*
-this.getMateriaal = function () {
-return zakmateriaal;
-}
-
-this.getMerk = function () {
-return merk;
- }
-}
-
- this.berekenSnoepTotaal = function (aantal) {
-   let totaalBedrag = 0;
-     totaalBedrag =  prijs * aantal;
- }
-
- this.getTotaal = function () {
-     return totaalBedrag;
- }
-
- this.berekenSnoepTotaal(aantal);
- 
- */
-
-
- /*
-function Koek(aantal) {
-}
-
-function Chocola(aantal) {
-}
-*/
-/*
-function Klant() {
- let winkelmand = new Winkelmand();
-
- this.wijzigAankopen = function (snoep, chocola, koek) {
-    winkelmand.vulWinkelmand(snoep, chocola, koek);
- };
-
- this.teBetalen = function () {
-     return winkelmand.getTotaalBedrag();
- }
-}
-
-
-let klant = new Klant();
-klant.wijzigAankopen(20,1,1);
-console.log('Totaalbedrag in winkelmand: ' + klant.teBetalen());
-
-let dagomzet = new Dagomzet();
-console.log('De totale dagomzet bedraagt: ' + 0 );
-*/
