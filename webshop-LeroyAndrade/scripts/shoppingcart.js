7ddbc628fe6218dfacd9fb13cc433b58ebea7ec0
AndradeLeroy <47416005+AndradeLeroy@users.noreply.github.com>
2020-11-17 23:18:12 +0100

    Alle opdrachten voltooid

--- webshop-LeroyAndrade/scripts/shoppingcart.js
+++ webshop-LeroyAndrade/scripts/shoppingcart.js
@@ -26,32 +26,67 @@ function Winkelmand() {
  this.setWinkelmandBedrag = function (snoep, chocola, koek) {
      totaalBedrag = totaalBedrag + snoep.getTotaal();
  }
-
 }
 
 function Snoep(){
-const prijs = 2.45;
-const soort = ['Hazelnoot', 'original'];
-const zakinhoud = "500mg";
-//const zakmateriaal = "plastic";
-//const merk = 'Ritter Sport';
 
+const prijs = 2.45;
+const soort = ['original', 'Whole Hazelnoot', 'White whole hazalnuts'];
+const zakinhoud = "500mg";
+const merk = 'Ritter Sport';
+
+ //dit weergeeft de prijs van het object Snoep
  this.getPrijs = function () {
+ //return `De prijs is €${prijs},- `;
  return prijs;
  }
 
- this.getSoort = function () {
+ //dit weergeeft alle namen die in de array zitten met de type / soort van de chocolade
+ this.getAllChocoNames = function () {
  return soort;
  }
+ 
+ //Hiermee wordt de soort / type van de chocolade geselecteerd, het nummer voer je in wanneer een NEW instantie wordt aangemaakt
+  this.getSoortChoco = function (i) {
+  return soort[i];
+  }
 
- this.getInhoud = function () {
+ //dit weergeeft de Product naam
+ this.getMerk = function () {
+  return merk;
+  }
+
+ //dit weergeeft de inhoud in de miligram hoeveelheid
+ this.getGewicht = function () {
  return zakinhoud;
  }
+
 }
 
 
+//Koppel de variabel snoep, aan het Object Snoep
 let snoep = new Snoep();
-console.log(snoep.getSoort());
+console.log(snoep.getPrijs());
+
+
+console.table(snoep.getMerk());
+
+                              //0 || 1 || 2
+console.log(snoep.getSoortChoco(0));
+
+
+console.table(snoep.getAllChocoNames());
+
+
+console.table(snoep.getGewicht());
+
+
+
+
+
+
+
+
 
 /*
 this.getMateriaal = function () {
