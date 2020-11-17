ae09bb97553741296ecfb8bef3b308f976d57bc6
AndradeLeroy <47416005+AndradeLeroy@users.noreply.github.com>
2020-11-17 11:07:52 +0100

    Werkende snoep.getSoort()
    Succesvol via een  Getter functiedata opgehaald

--- webshop-LeroyAndrade/scripts/shoppingcart.js
+++ webshop-LeroyAndrade/scripts/shoppingcart.js
@@ -1,76 +1,102 @@
 function Dagomzet() {
-    let omzet = 0;
+ let omzet = 0;
 
-    this.voegToeAanDagomzet = function (klantTotaal) {
-        omzet = omzet + klantTotaal;
-    }
+ this.voegToeAanDagomzet = function (klantTotaal) {
+     omzet = omzet + klantTotaal;
+ }
 
 }
 
 function Winkelmand() {
-    let totaalBedrag = 0;
+ let totaalBedrag = 0;
 
-    this.getTotaalBedrag = function () {
-        return totaalBedrag;
-    }
+ this.getTotaalBedrag = function () {
+     return totaalBedrag;
+ }
 
-    this.vulWinkelmand = function (aantalSnoep, aantalChocola, aantalKoek) {
+ this.vulWinkelmand = function (aantalSnoep, aantalChocola, aantalKoek) {
 
-        let snoep = new Snoep(aantalSnoep);
-        let chocola = new Chocola(aantalChocola);
-        let koek = new Koek(aantalKoek);
+     let snoep = new Snoep(aantalSnoep);
+     let chocola = new Chocola(aantalChocola);
+     let koek = new Koek(aantalKoek);
 
-        this.setWinkelmandBedrag(snoep, chocola, koek);
-    }
+     this.setWinkelmandBedrag(snoep, chocola, koek);
+ }
 
-    this.setWinkelmandBedrag = function (snoep, chocola, koek) {
-        totaalBedrag = totaalBedrag + snoep.getTotaal();
-    }
+ this.setWinkelmandBedrag = function (snoep, chocola, koek) {
+     totaalBedrag = totaalBedrag + snoep.getTotaal();
+ }
 
 }
 
-function Snoep(aantal) {
-    this.item = item;
+function Snoep(){
+const prijs = 2.45;
+const soort = ['Hazelnoot', 'original'];
+const zakinhoud = "500mg";
+//const zakmateriaal = "plastic";
+//const merk = 'Ritter Sport';
 
-    item.prijs = prijs;// let prijs = 2.45;
-    item.smaak = smaak;
-    item.naam = naam;
-    item.editie = editie;
+ this.getPrijs = function () {
+ return prijs;
+ }
 
-    console.log(this.prijs)
+ this.getSoort = function () {
+ return soort;
+ }
+
+ this.getInhoud = function () {
+ return zakinhoud;
+ }
 }
 
+
+let snoep = new Snoep();
+console.log(snoep.getSoort());
 
 /*
-    this.berekenSnoepTotaal = function (aantal) {
-      let totaalBedrag = 0;
-        totaalBedrag =  prijs * aantal;
-    }
+this.getMateriaal = function () {
+return zakmateriaal;
+}
 
-    this.getTotaal = function () {
-        return totaalBedrag;
-    }
+this.getMerk = function () {
+return merk;
+ }
+}
 
-    this.berekenSnoepTotaal(aantal);
-    
-    * /
+ this.berekenSnoepTotaal = function (aantal) {
+   let totaalBedrag = 0;
+     totaalBedrag =  prijs * aantal;
+ }
+
+ this.getTotaal = function () {
+     return totaalBedrag;
+ }
+
+ this.berekenSnoepTotaal(aantal);
+ 
+ */
+
+
+ /*
 function Koek(aantal) {
 }
 
 function Chocola(aantal) {
 }
-
+*/
+/*
 function Klant() {
-    let winkelmand = new Winkelmand();
+ let winkelmand = new Winkelmand();
 
-    this.wijzigAankopen = function (snoep, chocola, koek) {
-       winkelmand.vulWinkelmand(snoep, chocola, koek);
-    };
+ this.wijzigAankopen = function (snoep, chocola, koek) {
+    winkelmand.vulWinkelmand(snoep, chocola, koek);
+ };
 
-    this.teBetalen = function () {
-        return winkelmand.getTotaalBedrag();
-    }
+ this.teBetalen = function () {
+     return winkelmand.getTotaalBedrag();
+ }
 }
+
 
 let klant = new Klant();
 klant.wijzigAankopen(20,1,1);
@@ -78,3 +104,4 @@ console.log('Totaalbedrag in winkelmand: ' + klant.teBetalen());
 
 let dagomzet = new Dagomzet();
 console.log('De totale dagomzet bedraagt: ' + 0 );
+*/
